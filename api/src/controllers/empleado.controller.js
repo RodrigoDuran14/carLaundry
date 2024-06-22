5d9b8896bc8d85247bca7e3343adb70758b335e5
Rodrigo Martin Durán <potro_duran96@hotmail.com>
2024-06-22 02:52:37 -0300

    se creo las rutas de activarempleado, de adminempleado y para crear la contraseña si el empleado es admin

--- api/src/controllers/empleado.controller.js
+++ api/src/controllers/empleado.controller.js
@@ -1,6 +1,6 @@
 const EmpleadosModel = require("../models/Empleado.model");
-const LavadosModel = require("../models/lavado.model")
-
+const LavadosModel = require("../models/lavado.model");
+const bcrypt = require("bcrypt");
 
 const postEmpleado = async (req, res, next) => {
   try {
@@ -13,37 +13,40 @@ const postEmpleado = async (req, res, next) => {
       celular,
     });
 
-    await newEmpleado.save()
-    res.status(201).send(newEmpleado)
+    await newEmpleado.save();
+    res.status(201).send(newEmpleado);
   } catch (error) {
     next(error);
   }
 };
 
-const getEmpleadoList = async (req,res,next)=>{
+const getEmpleadoList = async (req, res, next) => {
   try {
-    const allEmpleados = await EmpleadosModel.find()
+    const allEmpleados = await EmpleadosModel.find();
 
-    res.status(200).send(allEmpleados)
+    res.status(200).send(allEmpleados);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-const getEmpleadoById = async (req, res,next) =>{
+const getEmpleadoById = async (req, res, next) => {
   try {
-    const {id}= req.params
-    const empleado = await EmpleadosModel.findById({_id: id}).populate("lavados", "horarioInicio horarioFin total estadoDelLavado")
+    const { id } = req.params;
+    const empleado = await EmpleadosModel.findById({ _id: id }).populate(
+      "lavados",
+      "horarioInicio horarioFin total estadoDelLavado"
+    );
     //falta agregar que traiga la info de lavados, una vez que se le asigne un lavado
-    !empleado ? res.status(404).send() : res.status(200).send(empleado)
+    !empleado ? res.status(404).send() : res.status(200).send(empleado);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-const findEmpleado = async (req,res,next)=>{
+const findEmpleado = async (req, res, next) => {
   try {
-    const {nombre, dni, mail, celular} = req.query
+    const { nombre, dni, mail, celular } = req.query;
 
     let empleadoQuery = {};
     if (nombre) empleadoQuery.nombre = new RegExp(nombre, "i");
@@ -51,23 +54,23 @@ const findEmpleado = async (req,res,next)=>{
     if (dni) empleadoQuery.dni = Number(dni);
     if (celular) empleadoQuery.celular = Number(celular);
 
-    const empleado = await EmpleadosModel.find(empleadoQuery)
+    const empleado = await EmpleadosModel.find(empleadoQuery);
 
-    res.status(200).send(empleado)
-
+    res.status(200).send(empleado);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-const findEmpleadoByLavadosDate = async (req,res,next)=>{
+const findEmpleadoByLavadosDate = async (req, res, next) => {
   try {
-    const {horarioInicio, horarioFin, estadoDelLavado} = req.query
+    const { horarioInicio, horarioFin, estadoDelLavado } = req.query;
 
     let lavadoQuery = {};
     if (horarioInicio) lavadoQuery.horarioInicio = Date(horarioInicio);
     if (horarioFin) lavadoQuery.horarioFin = new RegExp(horarioFin, "i");
-    if (estadoDelLavado) lavadoQuery.estadoDelLavado = new RegExp(estadoDelLavado, "i");
+    if (estadoDelLavado)
+      lavadoQuery.estadoDelLavado = new RegExp(estadoDelLavado, "i");
 
     if (Object.keys(lavadoQuery).length === 0) {
       return res.status(400).send({
@@ -86,33 +89,121 @@ const findEmpleadoByLavadosDate = async (req,res,next)=>{
     }
 
     const empleados = await EmpleadosModel.find({
-      lavados: { $in: lavadoId }
-
-    })
+      lavados: { $in: lavadoId },
+    });
     //.populate("lavados", "horarioInicio horarioFin estadoDelLavado vehiculoId clienteId");
 
     res.status(200).send(empleados);
-
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-const updateEmpleado = async (req,res,next)=>{
+const updateEmpleado = async (req, res, next) => {
   try {
-    const {id} = req.params
-    const update = req.body
+    const { id } = req.params;
+    const update = req.body;
 
-    const updateEmpleado = await EmpleadosModel.findByIdAndUpdate(id, {$set: update})
+    const updateEmpleado = await EmpleadosModel.findByIdAndUpdate(id, {
+      $set: update,
+    });
 
-    if(!updateEmpleado){
-      return res.status(404).send({error: "Empleado no encontrado"})
+    if (!updateEmpleado) {
+      return res.status(404).send({ error: "Empleado no encontrado" });
     }
 
-    res.status(200).send(update)
+    res.status(200).send(update);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-module.exports = {postEmpleado, getEmpleadoList, getEmpleadoById, findEmpleado, findEmpleadoByLavadosDate, updateEmpleado }
+const updateActiveEmpleado = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const empleado = await EmpleadosModel.findById(id);
+
+    if (!empleado) {
+      return res.status(404).send({ error: "Empleado no encontrado" });
+    }
+
+    empleado.activo = !empleado.activo;
+
+    await empleado.save();
+    res.status(200).send(empleado);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateAdminEmpleado = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const empleado = await EmpleadosModel.findById(id);
+
+    if (!empleado) {
+      return res.status(404).send({ error: "Empleado no encontrado" });
+    }
+
+    if(empleado.admin){
+      empleado.password = ""
+    }
+
+    empleado.admin = !empleado.admin;
+
+    await empleado.save();
+    res.status(200).send(empleado);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createPassword = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const {password} = req.body;
+
+    if(!password) {
+      return res.status(400).send("El campo password es obligatorio")
+    }
+    
+    const empleado = await EmpleadosModel.findById(id);
+    console.log(empleado.admin)
+
+    if (!empleado) {
+      return res.status(404).send({ error: "Empleado no encontrado" });
+    }
+
+    if (!empleado.admin) {
+        return res
+        .status(403)
+        .send("Acceso denegado. El empleado no es administrador");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    empleado.password = hashedPassword;
+
+    await empleado.save();
+    res
+      .status(200)
+      .send({
+        message: "Contraseña actualizada correctamente",
+        empleado: { id: empleado._id, nombre: empleado.nombre },
+      });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  postEmpleado,
+  getEmpleadoList,
+  getEmpleadoById,
+  findEmpleado,
+  findEmpleadoByLavadosDate,
+  updateEmpleado,
+  updateActiveEmpleado,
+  updateAdminEmpleado,
+  createPassword
+};
--- api/src/routes/empleado.route.js
+++ api/src/routes/empleado.route.js
@@ -1,12 +1,25 @@
-const express = require("express")
-const router = express.Router()
-const {postEmpleado,getEmpleadoList,getEmpleadoById,findEmpleado,findEmpleadoByLavadosDate} = require("../controllers/empleado.controller")
+const express = require("express");
+const router = express.Router();
+const {
+  postEmpleado,
+  getEmpleadoList,
+  getEmpleadoById,
+  findEmpleado,
+  findEmpleadoByLavadosDate,
+  updateEmpleado,
+  updateAdminEmpleado,
+  updateActiveEmpleado,
+  createPassword
+} = require("../controllers/empleado.controller");
 
-router.get("/empleados", getEmpleadoList)
-router.get("/empleado/:id", getEmpleadoById)
-router.get("/empleado", findEmpleado)
-router.get("/empleadolavado", findEmpleadoByLavadosDate)
+router.get("/empleados", getEmpleadoList);
+router.get("/empleado/:id", getEmpleadoById);
+router.get("/empleado", findEmpleado);
+router.get("/empleadolavado", findEmpleadoByLavadosDate);
+router.put("/empleados/:id", updateEmpleado);
+router.put("/empleadopassword/:id", createPassword)
+router.post("/empleado", postEmpleado);
+router.patch("/empleado/:id", updateActiveEmpleado);
+router.patch("/empleadoadmin/:id", updateAdminEmpleado);
 
-router.post("/empleado", postEmpleado)
-
-module.exports = router
+module.exports = router;
